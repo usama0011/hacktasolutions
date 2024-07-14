46f5b1b005445d41b419b3d78e6028a6f99799f3
unknown <za5232208@gmail.com>
2024-07-15 03:57:22 +0500

    udpate me

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -18,7 +18,9 @@ connectDB();
 // Routes
 
 app.use("/api/auth", authRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Welcome to Server side of Hackta Solutions");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
